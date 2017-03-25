5a622e9e16a19e49be1efc71fb674cfaad9c42a3
fabiorubim <fabiorubim@hotmail.com>
2017-03-25 06:29:23 -0700

    Finalizado o assunto sobre rotas.

--- Rotas/DemosRotaMVC/DemosRotaMVC/App_Start/RouteConfig.cs
+++ Rotas/DemosRotaMVC/DemosRotaMVC/App_Start/RouteConfig.cs
@@ -14,9 +14,27 @@ namespace DemosRotaMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Todas",
+                url: "Todas",
+                defaults: new { controller = "Noticias", action = "Todas" }
+            );
+
+            routes.MapRoute(
+                name: "Por Categoria",
+                url: "{cat}",
+                defaults: new { controller = "Noticias", action = "NoticiasDaCategoria"}
+            );
+
+            routes.MapRoute(
+                name: "Mostrar Noticia",
+                url: "{categoria}/{titulo}/{id}",
+                defaults: new { controller = "Noticias", action = "MostrarNoticia"}
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Noticias", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
