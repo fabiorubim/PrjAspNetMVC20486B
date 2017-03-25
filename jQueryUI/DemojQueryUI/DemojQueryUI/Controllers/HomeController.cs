b5a251f929168e46a008a91b8a80b9a25b3405f2
fabiorubim <fabiorubim@hotmail.com>
2017-03-25 09:46:48 -0700

    Usando JQueryUI

---
+++ jQueryUI/DemojQueryUI/DemojQueryUI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemojQueryUI.Models;
+
+namespace DemojQueryUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetData()
+        { 
+            IList<Linguagem> linguagens = null;
+            using (var ctx = new LinguagemDB())
+            {
+                linguagens = ctx.Linguagens.ToList();
+            }
+
+            return Json(linguagens.Select(x => x.Nome),JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ jQueryUI/DemojQueryUI/DemojQueryUI/Models/LinguagemDB.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DemojQueryUI.Models
+{
+    public class LinguagemDB:DbContext
+    {
+        public LinguagemDB():base("LinguagemConn")
+        {
+            Database.SetInitializer(new Carga());
+        }
+
+        public DbSet<Linguagem> Linguagens { get; set; }       
+    }
+
+    public class Carga : DropCreateDatabaseIfModelChanges<LinguagemDB>
+    {
+        protected override void Seed(LinguagemDB context)
+        {
+            var linguagens = new List<Linguagem>
+            {
+              new Linguagem{Nome="ActionScript"},
+              new Linguagem{Nome="AppleScript"},
+              new Linguagem{Nome="Asp"},
+              new Linguagem{Nome="BASIC"},
+              new Linguagem{Nome="C"},
+              new Linguagem{Nome="C++"},
+              new Linguagem{Nome="Clojure"},
+              new Linguagem{Nome="COBOL"},
+              new Linguagem{Nome="ColdFusion"},
+              new Linguagem{Nome="C#"},
+              new Linguagem{Nome="Erlang"},
+              new Linguagem{Nome="Fortran"},
+              new Linguagem{Nome="Groovy"},
+              new Linguagem{Nome="Haskell"},
+              new Linguagem{Nome="Java"},
+              new Linguagem{Nome="JavaScript"},              
+              new Linguagem{Nome="Lisp"},
+              new Linguagem{Nome="Perl"},
+              new Linguagem{Nome="PHP"},
+              new Linguagem{Nome="Python"},
+              new Linguagem{Nome="Ruby"},
+              new Linguagem{Nome="Scala"},
+              new Linguagem{Nome="Scheme"}
+            };
+
+            context.Linguagens.AddRange(linguagens);
+        }
+    }
+}
