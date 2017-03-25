832c59b5b54fddd51150957a61e44b74331a6a33
fabiorubim <fabiorubim@hotmail.com>
2017-03-25 11:27:32 -0700

    Um pouco de WebApi

---
+++ WebApi/DemoApi/DemoApi/Controllers/LivrosController.cs
@@ -0,0 +1,63 @@
+﻿using DemoApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoApi.Controllers
+{
+    public class LivrosController : ApiController
+    {
+        private readonly LivroDB _ctx = new LivroDB();
+        public Livro Post(Livro livro)
+        {
+            if (ModelState.IsValid)
+            {
+                _ctx.Livros.Add(livro);
+                _ctx.SaveChanges();                
+            }
+
+            return livro;
+        }
+
+        public IEnumerable<Livro> Get()
+        {
+            return _ctx.Livros.ToList();
+        }
+
+        public Livro Get(int Id)
+        {
+            return _ctx.Livros.Find(Id);
+        }
+
+        public HttpResponseMessage Put(int id, Livro livro)
+        {
+            if (id != livro.Id)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            else
+            {
+                _ctx.Entry(livro).State = System.Data.EntityState.Modified;
+                _ctx.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.Accepted,livro);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var livro = _ctx.Livros.Find(id);
+            _ctx.Livros.Remove(livro);
+            _ctx.SaveChanges();
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            _ctx.Dispose();
+        }
+
+    }
+}
---
+++ WebApi/DemoApi/DemoApi/Models/LivroDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DemoApi.Models
+{
+    public class LivroDB:DbContext
+    {
+        public LivroDB():base("LivroConn")
+        {
+
+        }
+
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
